fab8483a096ed1233897636d102d44120f04f6a5
mdalifich <vasjapupkin.2020@mail.ru>
2023-12-05 20:25:46 +0300

    Mыыыы

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import random
 import sys
 
 from PyQt5 import uic
@@ -12,65 +13,123 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>835</width>
-    <height>600</height>
+    <width>343</width>
+    <height>345</height>
    </rect>
   </property>
   <property name="windowTitle">
    <string>MainWindow</string>
   </property>
   <widget class="QWidget" name="centralwidget">
-   <widget class="QCalendarWidget" name="calendarWidget">
+   <widget class="QLabel" name="KolVoStones">
     <property name="geometry">
      <rect>
       <x>0</x>
-      <y>50</y>
-      <width>501</width>
-      <height>471</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QTimeEdit" name="timeEdit">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>10</y>
-      <width>501</width>
-      <height>31</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QLineEdit" name="lineEdit">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>530</y>
-      <width>501</width>
-      <height>31</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QPushButton" name="addEventBtn">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>560</y>
-      <width>501</width>
-      <height>31</height>
+      <y>0</y>
+      <width>81</width>
+      <height>21</height>
      </rect>
     </property>
     <property name="text">
-     <string>Add Event</string>
+     <string>Kol-vo stones</string>
     </property>
    </widget>
-   <widget class="QListWidget" name="eventList">
+   <widget class="QSpinBox" name="stones">
     <property name="geometry">
      <rect>
-      <x>510</x>
-      <y>10</y>
-      <width>321</width>
-      <height>581</height>
+      <x>90</x>
+      <y>0</y>
+      <width>211</width>
+      <height>24</height>
      </rect>
+    </property>
+   </widget>
+   <widget class="QLCDNumber" name="remainLcd">
+    <property name="geometry">
+     <rect>
+      <x>0</x>
+      <y>30</y>
+      <width>341</width>
+      <height>23</height>
+     </rect>
+    </property>
+   </widget>
+   <widget class="QLabel" name="SkolkoVsatStone">
+    <property name="geometry">
+     <rect>
+      <x>0</x>
+      <y>70</y>
+      <width>81</width>
+      <height>21</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>SkolkoStone?</string>
+    </property>
+   </widget>
+   <widget class="QSpinBox" name="takeInput">
+    <property name="geometry">
+     <rect>
+      <x>90</x>
+      <y>70</y>
+      <width>251</width>
+      <height>24</height>
+     </rect>
+    </property>
+   </widget>
+   <widget class="QPushButton" name="takeButton">
+    <property name="geometry">
+     <rect>
+      <x>0</x>
+      <y>100</y>
+      <width>341</width>
+      <height>23</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>PushButton</string>
+    </property>
+   </widget>
+   <widget class="QPushButton" name="startButton">
+    <property name="geometry">
+     <rect>
+      <x>310</x>
+      <y>0</y>
+      <width>31</width>
+      <height>23</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>edit</string>
+    </property>
+   </widget>
+   <widget class="QListWidget" name="listWidget">
+    <property name="geometry">
+     <rect>
+      <x>0</x>
+      <y>130</y>
+      <width>341</width>
+      <height>191</height>
+     </rect>
+    </property>
+   </widget>
+   <widget class="QLabel" name="resultLabel">
+    <property name="geometry">
+     <rect>
+      <x>0</x>
+      <y>320</y>
+      <width>341</width>
+      <height>21</height>
+     </rect>
+    </property>
+    <property name="layoutDirection">
+     <enum>Qt::LeftToRight</enum>
+    </property>
+    <property name="text">
+     <string/>
+    </property>
+    <property name="alignment">
+     <set>Qt::AlignCenter</set>
     </property>
    </widget>
   </widget>
@@ -81,59 +140,51 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
 '''
 
 
-class SimplePlanner(QMainWindow):
+class Pseudonym(QMainWindow):
     def __init__(self):
         super().__init__()
         f = io.StringIO(template)
         uic.loadUi(f, self)
-        self.addEventBtn.clicked.connect(self.Plan)
-        self.text = ''
-        self.list1 = []
+        self.startButton.clicked.connect(self.newGame)
+        self.takeButton.clicked.connect(self.Move)
+        self.count = 0
+        self.count2 = 0
 
-    def Plan(self):
-        if len(str(self.timeEdit.time().hour())) == 1:
-            self.text += '0' + str(self.timeEdit.time().hour())
+    def newGame(self):
+        self.listWidget.clear()
+        self.remainLcd.display(self.stones.text())
+        self.count = int(self.stones.text())
+        self.resultLabel.setText('')
+
+    def Move(self):
+        if self.count > 0:
+            self.listWidget.addItem(self.takeInput.text())
+            self.remainLcd.display(str(self.count - int(self.takeInput.text())))
+            self.count -= int(self.takeInput.text())
+            if self.count <= 0:
+                self.resultLabel.setText('Победа игрока')
         else:
-            self.text += str(self.timeEdit.time().hour())
-        if len(str(self.timeEdit.time().minute())) == 1:
-            self.text += ':' + '0' + str(self.timeEdit.time().minute())
+            self.resultLabel.setText('Победа компьютера')
+
+        if self.count > 0:
+            if self.count <= 3:
+                self.count2 = self.count
+                self.listWidget.addItem(str(self.count2))
+                self.remainLcd.display(str(self.count - self.count2))
+                self.count -= self.count2
+            else:
+                self.count2 = random.randint(1, 3)
+                self.listWidget.addItem(str(self.count2))
+                self.remainLcd.display(str(self.count - self.count2))
+                self.count -= self.count2
+            if self.count <= 0:
+                self.resultLabel.setText('Победа компьютера')
         else:
-            self.text += ':' + str(self.timeEdit.time().minute())
-        if len(str(self.timeEdit.time().second())) == 1:
-            self.text += ':' + '0' + str(self.timeEdit.time().second())
-        else:
-            self.text += ':' + str(self.timeEdit.time().second())
-
-        if len(str(self.calendarWidget.selectedDate().month())) == 1 and len(
-                str(self.calendarWidget.selectedDate().day())) == 1:
-            self.list1.append(str(self.calendarWidget.selectedDate().year()) + '-' + '0' + str(
-                self.calendarWidget.selectedDate().month()) + '-' + '0' + str(
-                self.calendarWidget.selectedDate().day()) + ' ' + self.text + ' - ' + self.lineEdit.text())
-
-        elif len(str(self.calendarWidget.selectedDate().month())) == 1:
-            self.list1.append(str(self.calendarWidget.selectedDate().year()) + '-' + '0' + str(
-                self.calendarWidget.selectedDate().month()) + '-' + str(
-                self.calendarWidget.selectedDate().day()) + ' ' + self.text + ' - ' + self.lineEdit.text())
-
-        elif len(str(self.calendarWidget.selectedDate().day())) == 1:
-            self.list1.append(str(self.calendarWidget.selectedDate().year()) + '-' + str(
-                self.calendarWidget.selectedDate().month()) + '-' + '0' + str(
-                self.calendarWidget.selectedDate().day()) + ' ' + self.text + ' - ' + self.lineEdit.text())
-
-        else:
-            self.list1.append(str(self.calendarWidget.selectedDate().year()) + '-' + str(
-                self.calendarWidget.selectedDate().month()) + '-' + str(
-                self.calendarWidget.selectedDate().day()) + ' ' + self.text + ' - ' + self.lineEdit.text())
-
-        self.list1.sort()
-        self.eventList.clear()
-        for i in self.list1:
-            self.eventList.addItem(i)
-        self.text = ''
+            self.resultLabel.setText('Победа игрока')
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = SimplePlanner()
+    ex = Pseudonym()
     ex.show()
     sys.exit(app.exec_())
