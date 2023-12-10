e0a7f2344cfec865d6e465b528e0096951e281e4
mdalifich <vasjapupkin.2020@mail.ru>
2023-12-10 16:55:17 +0300

    Mыыыы

--- main.py
+++ main.py
@@ -1,190 +1,176 @@
 import io
 import random
 import sys
+import math
 
 from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import QRectF, QPointF
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 template = '''<?xml version="1.0" encoding="UTF-8"?>
 <ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
+ <class>Form</class>
+ <widget class="QWidget" name="Form">
   <property name="geometry">
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>343</width>
-    <height>345</height>
+    <width>800</width>
+    <height>900</height>
    </rect>
   </property>
   <property name="windowTitle">
-   <string>MainWindow</string>
+   <string>Form</string>
   </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QLabel" name="KolVoStones">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>0</y>
-      <width>81</width>
-      <height>21</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Kol-vo stones</string>
-    </property>
-   </widget>
-   <widget class="QSpinBox" name="stones">
-    <property name="geometry">
-     <rect>
-      <x>90</x>
-      <y>0</y>
-      <width>211</width>
-      <height>24</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QLCDNumber" name="remainLcd">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>30</y>
-      <width>341</width>
-      <height>23</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QLabel" name="SkolkoVsatStone">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>70</y>
-      <width>81</width>
-      <height>21</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>SkolkoStone?</string>
-    </property>
-   </widget>
-   <widget class="QSpinBox" name="takeInput">
-    <property name="geometry">
-     <rect>
-      <x>90</x>
-      <y>70</y>
-      <width>251</width>
-      <height>24</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QPushButton" name="takeButton">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>100</y>
-      <width>341</width>
-      <height>23</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>PushButton</string>
-    </property>
-   </widget>
-   <widget class="QPushButton" name="startButton">
-    <property name="geometry">
-     <rect>
-      <x>310</x>
-      <y>0</y>
-      <width>31</width>
-      <height>23</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>edit</string>
-    </property>
-   </widget>
-   <widget class="QListWidget" name="listWidget">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>130</y>
-      <width>341</width>
-      <height>191</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QLabel" name="resultLabel">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>320</y>
-      <width>341</width>
-      <height>21</height>
-     </rect>
-    </property>
-    <property name="layoutDirection">
-     <enum>Qt::LeftToRight</enum>
-    </property>
-    <property name="text">
-     <string/>
-    </property>
-    <property name="alignment">
-     <set>Qt::AlignCenter</set>
-    </property>
-   </widget>
+  <widget class="QPushButton" name="btn">
+   <property name="geometry">
+    <rect>
+     <x>10</x>
+     <y>20</y>
+     <width>221</width>
+     <height>41</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>Показать</string>
+   </property>
+  </widget>
+  <widget class="QLabel" name="label">
+   <property name="geometry">
+    <rect>
+     <x>270</x>
+     <y>20</y>
+     <width>57</width>
+     <height>21</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>side:</string>
+   </property>
+  </widget>
+  <widget class="QLabel" name="label_2">
+   <property name="geometry">
+    <rect>
+     <x>270</x>
+     <y>50</y>
+     <width>57</width>
+     <height>21</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>coeff:</string>
+   </property>
+  </widget>
+  <widget class="QLabel" name="label_3">
+   <property name="geometry">
+    <rect>
+     <x>270</x>
+     <y>80</y>
+     <width>57</width>
+     <height>21</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>n:</string>
+   </property>
+  </widget>
+  <widget class="QLineEdit" name="lineEdit">
+   <property name="geometry">
+    <rect>
+     <x>310</x>
+     <y>20</y>
+     <width>471</width>
+     <height>23</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>300</string>
+   </property>
+  </widget>
+  <widget class="QLineEdit" name="lineEdit_2">
+   <property name="geometry">
+    <rect>
+     <x>310</x>
+     <y>50</y>
+     <width>471</width>
+     <height>23</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>0.9</string>
+   </property>
+  </widget>
+  <widget class="QLineEdit" name="lineEdit_3">
+   <property name="geometry">
+    <rect>
+     <x>310</x>
+     <y>80</y>
+     <width>471</width>
+     <height>23</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>5</string>
+   </property>
   </widget>
  </widget>
  <resources/>
  <connections/>
-</ui>
-'''
+</ui>'''
 
 
-class Pseudonym(QMainWindow):
+class Square1(QMainWindow):
     def __init__(self):
         super().__init__()
         f = io.StringIO(template)
         uic.loadUi(f, self)
-        self.startButton.clicked.connect(self.newGame)
-        self.takeButton.clicked.connect(self.Move)
-        self.count = 0
-        self.count2 = 0
+        self.xUp, self.yUp = 50, 130
+        self.box = 300
+        self.color = QColor(250, 0, 0)
+        self.rot = 0
+        self.initUi()
 
-    def newGame(self):
-        self.listWidget.clear()
-        self.remainLcd.display(self.stones.text())
-        self.count = int(self.stones.text())
-        self.resultLabel.setText('')
+    def initUi(self):
+        self.btn.clicked.connect(self.paint)
+        self.do_paint = False
 
-    def Move(self):
-        if self.count > 0:
-            self.listWidget.addItem(self.takeInput.text())
-            self.remainLcd.display(str(self.count - int(self.takeInput.text())))
-            self.count -= int(self.takeInput.text())
-            if self.count <= 0:
-                self.resultLabel.setText('Победа игрока')
-        else:
-            self.resultLabel.setText('Победа компьютера')
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
 
-        if self.count > 0:
-            if self.count <= 3:
-                self.count2 = self.count
-                self.listWidget.addItem(str(self.count2))
-                self.remainLcd.display(str(self.count - self.count2))
-                self.count -= self.count2
-            else:
-                self.count2 = random.randint(1, 3)
-                self.listWidget.addItem(str(self.count2))
-                self.remainLcd.display(str(self.count - self.count2))
-                self.count -= self.count2
-            if self.count <= 0:
-                self.resultLabel.setText('Победа компьютера')
-        else:
-            self.resultLabel.setText('Победа игрока')
+    def paint(self, event):
+        self.do_paint = True
+        self.repaint()
+
+    def draw(self, qp):
+        self.rot = 0
+        self.xUp, self.yUp = -150, -150
+        self.XX = self.xUp + int(self.lineEdit.text()) // 2
+        self.YY = self.yUp + int(self.lineEdit.text()) // 2
+        self.box = int(self.lineEdit.text())
+        qp.setPen(self.color)
+        qp.translate(self.XX + 300, self.YY + 300)
+        self.maxu = 300 * float(self.lineEdit_2.text())
+        self.minu = 300 - (300 * float(self.lineEdit_2.text()))
+        for _ in range(int(self.lineEdit_3.text())):
+            self.Rect = QRectF(self.xUp, self.yUp, self.box, self.box)
+            self.box *= float(self.lineEdit_2.text())
+            self.xUp = self.XX - self.box / 2
+            self.yUp = self.YY - self.box / 2
+
+            qp.drawRect(self.Rect)
+
+
+            qp.rotate(self.rot)
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Pseudonym()
+    ex = Square1()
     ex.show()
     sys.exit(app.exec_())
