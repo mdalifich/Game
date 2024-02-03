4457bacae512a8453119557029e958f0fd341716
mdalifich <vasjapupkin.2020@mail.ru>
2024-02-03 11:04:15 +0300

    Mыыыы

---
+++ curs.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame import *
+import os
+import load_image
+from load_image import *
+
+
+class Curs(pygame.sprite.Sprite):
+    def __init__(self, x, y, screen):
+        super().__init__()
+        self.x, self.y = x, y
+        self.image = load_image('data/cursor.png')
+        self.rect = Rect(self.x, self.y, 42, 42)
+        self.screen = screen
+
+    def draw(self):
+        self.screen.blit(self.image, (self.rect.x, self.rect.y))
--- main.py
+++ main.py
@@ -1,48 +1,59 @@
 import pygame
 from pygame import *
 import os
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-class Curs(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.x, self.y = x, y
-        self.image = load_image('data/cursor.png')
-        self.rect = Rect(self.x, self.y, 42, 42)
-
-    def draw(self):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
-        print(234)
+import load_image
+from load_image import *
+import curs
+from curs import *
+import Person
+from Person import *
 
 
 if __name__ == '__main__':
     pygame.init()
-    size, box = [int(i) for i in input().split()]
+    size = 500
     screen = pygame.display.set_mode((size, size))
     pygame.mouse.set_visible(False)
+    NewCurs = Curs(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], screen)
+    NewPers = Pers(0, 0, screen)
 
     while pygame.event.wait().type != pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            key = pygame.key.get_pressed()
+
+            if key[pygame.K_UP]:
+                NewPers.rect.y -= 10
+            if key[pygame.K_DOWN]:
+                NewPers.rect.y += 10
+            if key[pygame.K_RIGHT]:
+                NewPers.rect.x += 10
+            if key[pygame.K_LEFT]:
+                NewPers.rect.x -= 10
+
+            pygame.display.update()
         # Чтобы экран был черный сюда нада вписать (0, 0, 0)
         screen.fill((255, 255, 255))
+
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_UP]:
+            NewPers.rect.y -= 10
+        if key[pygame.K_DOWN]:
+            NewPers.rect.y += 10
+        if key[pygame.K_RIGHT]:
+            NewPers.rect.x += 10
+        if key[pygame.K_LEFT]:
+            NewPers.rect.x -= 10
+
+        NewPers.draw()
+
         if pygame.mouse.get_focused():
-            NewCurs = Curs(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+            NewCurs.rect.x, NewCurs.rect.y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
             NewCurs.draw()
+
 
         pygame.display.flip()
     pygame.quit()
